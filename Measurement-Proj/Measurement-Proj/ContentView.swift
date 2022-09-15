07d7960d9cfb46bc6c796fc038e90a389ef86035
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2022-09-15 11:42:15 -0300

    Initial commit.

---
+++ Measurement-Proj/Measurement-Proj/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  Measurement-Proj
+//
+//  Created by Rafael Carvalho on 15/09/22.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
